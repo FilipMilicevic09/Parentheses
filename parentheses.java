6bc263ff91846a21e338d7356b02bafb60930aad
FilipMilicevic09 <filip.milicevic009@gmail.com>
2025-11-11 19:29:06 +0000

    start

--- parentheses.java
+++ parentheses.java
@@ -1,1 +1,7 @@
-
+//"() [] {}" valid
+//"([])" valid
+// "([)]" not valid
+// 1) find the last ([{
+// 2) Check the item to the right
+// 3) Remove both and go again
+// 4) Keep going until string empty or determined invalid
